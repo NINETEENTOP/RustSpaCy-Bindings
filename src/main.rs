0475bf06635527679d6b99864a6194e40db2a119
NINETEENTOP <98999005+NINETEENTOP@users.noreply.github.com>
2024-04-15 08:50:33 +0000

    spi

--- src/main.rs
+++ src/main.rs
@@ -2,3 +2,8 @@
 #[path = "lib/spacy.rs"] mod spacy;
 
 use crate::spacy::doc::Callable;
+
+fn main() {
+  let spacy = spacy::Module::init();
+  spacy.load("en_core_web_lg");
+  let pangram1 = spacy::nlp("With tenure, Suzie’d have all the more leisure for yachting, but her publications are no good.");
